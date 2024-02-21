7b9ced91b7446ba2667a83ba3d8499797f35e0c3
Hassan <hhalnajj@uci.edu>
2024-02-21 14:47:51 -0800

    Default Xcode files

---
+++ HealthWars/HealthWars/HealthWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthWarsApp.swift
+//  HealthWars
+//
+//  Created by MAC on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
